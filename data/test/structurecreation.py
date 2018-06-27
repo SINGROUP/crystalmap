4856107b4c8a0948c385166708554325ae4caa25
Lauri Himanen <lauri.himanen@aalto.fi>
2018-06-27 14:57:06 +0300

    Added more slightly distorted geometries to test set.

--- data/test/structurecreation.py
+++ data/test/structurecreation.py
@@ -18,12 +18,27 @@ Diamond = ase.build.bulk(
     a=3.567,
 )
 
+Diamond2 = ase.build.bulk(
+    name="C",
+    crystalstructure="diamond",
+    a=3.567,
+)
+Diamond2.rattle(stdev=0.1)
+Diamond2.wrap()
+
 NaCl = ase.build.bulk(
     name="NaCl",
     crystalstructure="rocksalt",
     a=5.64,
 )
 NaCl.wrap()
+
+NaCl2 = ase.build.bulk(
+    name="NaCl",
+    crystalstructure="rocksalt",
+    a=6,
+)
+NaCl2.wrap()
 
 Al = ase.build.bulk(
     name="Al",
@@ -43,11 +58,25 @@ Cu = ase.build.bulk(
     a=3.597,
 )
 
+Cu2 = ase.build.bulk(
+    name="Cu",
+    crystalstructure="fcc",
+    a=3.20,
+)
+
 GaAs = ase.build.bulk(
     name="GaAs",
     crystalstructure="zincblende",
     a=5.653,
 )
+
+GaAs2 = ase.build.bulk(
+    name="GaAs",
+    crystalstructure="zincblende",
+    a=5.653,
+)
+GaAs2.rattle(stdev=0.1)
+GaAs2.wrap()
 
 SiC = ase.build.bulk(
     name="SiC",
@@ -61,6 +90,14 @@ CsCl = ase.build.bulk(
     crystalstructure="cesiumchloride",
     a=4.123,
 )
+
+CsCl2 = ase.build.bulk(
+    name="CsCl",
+    crystalstructure="cesiumchloride",
+    a=4.123,
+)
+CsCl2.rattle(stdev=0.1)
+CsCl2.wrap()
 
 # view(Si)
 # view(Diamond)
@@ -79,8 +116,14 @@ geoms = {
     "Diamond": {
         "atoms": Diamond
     },
+    "Diamond2": {
+        "atoms": Diamond2
+    },
     "NaCl": {
         "atoms": NaCl
+    },
+    "NaCl2": {
+        "atoms": NaCl2
     },
     "Al": {
         "atoms": Al
@@ -91,14 +134,23 @@ geoms = {
     "Cu": {
         "atoms": Cu
     },
+    "Cu2": {
+        "atoms": Cu2
+    },
     "GaAs": {
         "atoms": GaAs
+    },
+    "GaAs2": {
+        "atoms": GaAs2
     },
     "SiC": {
         "atoms": SiC
     },
     "CsCl": {
         "atoms": CsCl
+    },
+    "CsCl2": {
+        "atoms": CsCl2
     },
 }
 with open("test.pickle", "wb") as fout:
