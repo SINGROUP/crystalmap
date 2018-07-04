45b80065850615a727f76485750b885ec3ada053
Lauri Himanen <lauri.himanen@aalto.fi>
2018-07-04 14:57:50 +0300

    Added library declarations.

--- visualization/visualizer/src/app/app.component.ts
+++ visualization/visualizer/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var d3;
+declare var PIXI;
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.styl']
 })
 export class AppComponent {
-    title = 'app';
-    data string;
+    data:any;
 
     /*
      * Opens the file selected by the user, parses the JSON and saves contents
@@ -50,9 +51,9 @@ export class AppComponent {
 
         // Defines the simulation forces
         let simulation = d3.forceSimulation()
-            .force('link', d3.forceLink().id((d) => d.id))            // Harmonic spring
-            .force('charge', d3.forceManyBody())                      // Long-range charge
-            .force('center', d3.forceCenter(width / 2, height / 2));  // Centering force
+            .force('link', d3.forceLink().id((d) => d.id))              // Harmonic spring
+            .force('charge', d3.forceManyBody().strength(-30))          // Long-range charge
+            .force('center', d3.forceCenter(width / 2, height / 2));    // Centering force
 
         // A layer for drawing links
         let links = new PIXI.Graphics();
